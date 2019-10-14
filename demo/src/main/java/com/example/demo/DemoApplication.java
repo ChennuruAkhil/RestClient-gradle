ba0ab245258b1d37258749cdff2720fff67812ff
“ChennuruAkhil” <chennuri.akhil@gmail.com>
2019-10-14 09:51:08 +0530

    Added Swagger

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.example.demo.client.RestClient;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ComponentScan("com.example.demo")
+@EnableSwagger2
 public class DemoApplication {
 	
 
@@ -18,6 +24,14 @@ public class DemoApplication {
 		System.out.println("started");
 		RestClient r= new RestClient();
 		r.getRequest();
+	}
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+				.build();
 	}
 
 }
--- demo/src/main/java/com/example/demo/controller/RestController.java
+++ demo/src/main/java/com/example/demo/controller/RestController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.client.RestClient2;
 import com.example.demo.dto.Person;
 
 @org.springframework.web.bind.annotation.RestController
+
 public class RestController {
 	
 	@Autowired
